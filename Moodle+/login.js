3b056213d3d8746be06178250a7c0984deb709b0
Abhisar03 <109359297+Abhisar03@users.noreply.github.com>
2022-08-26 17:14:42 +0530

    Update login.js

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,68 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function solver3(z){
+    let ans = 0;
+    if(words[z+1] === 'subtract'){
+        ans = parseInt(words[z+2]) - parseInt(words[z+4])
+    }
+    
+    if(words[z+1] === 'add'){
+        ans = parseInt(words[z+2]) + parseInt(words[z+4])
+    }
+
+    if(words[z+1] === 'enter'){
+        if(words[z+2] === "first"){
+            ans = parseInt(words[z+4])
+        }
+        if(words[z+2] === "second"){
+            ans = parseInt(words[z+6])
+        }
+        
+    }
+
+
+
+return ans;
+}
+
+
+
+
+
+
+
+
+
+
+
+const login_element = document.querySelector("body"); // Fill the selector for the login element in ""
 let login_text = login_element.innerText;
 
-let question = ""; // Use split and array operations on the login_text string to extract the question
+const words = login_text.split(' ')
+let z = words.indexOf('username\nPlease')
 
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
+ans = solver3(z)
 
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
+let answer = ans; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
+
+const captcha_input_element = document.querySelector("#valuepkg3"); // Fill the selector for the captcha input element in ""
 captcha_input_element.value = answer;
